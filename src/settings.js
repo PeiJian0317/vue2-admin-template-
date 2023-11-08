8e38b1205e3b4169c1f9ad10ef42a968b680114a
PeiJian0317 <2567750155@qq.com>
2023-11-08 16:09:34 +0800

    初始化项目

---
+++ src/settings.js
@@ -0,0 +1,16 @@
+module.exports = {
+
+  title: 'Vue Admin Template', // 网站标题
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: true, // 是否固定头部
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: true // 侧边logo
+}

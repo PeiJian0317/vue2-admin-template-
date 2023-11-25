edac0f478998dd4eb95fae837cdcc64c5cd7dd43
PeiJian0317 <2567750155@qq.com>
2023-11-25 15:31:19 +0800

    其他模块的集成

--- src/router/index.js
+++ src/router/index.js
@@ -14,8 +14,30 @@ import roleRouter from './modules/role'
 import salaryRouter from './modules/salary'
 import socialRouter from './modules/social'
 
+/**
+ * Note: sub-menu only appear when route children.length >= 1
+ * Detail see: https://panjiachen.github.io/vue-element-admin-site/guide/essentials/router-and-nav.html
+ *
+ * hidden: true                   if set true, item will not show in the sidebar(default is false)
+ * alwaysShow: true               if set true, will always show the root menu
+ *                                if not set alwaysShow, when item has more than one children route,
+ *                                it will becomes nested mode, otherwise not show the root menu
+ * redirect: noRedirect           if set noRedirect will no redirect in the breadcrumb
+ * name:'router-name'             the name is used by <keep-alive> (must set!!!)
+ * meta : {
+    roles: ['admin','editor']    control the page roles (you can set multiple roles)
+    title: 'title'               the name show in sidebar and breadcrumb (recommend set)
+    icon: 'svg-name'/'el-icon-x' the icon show in the sidebar
+    breadcrumb: false            if set false, the item will hidden in breadcrumb(default is true)
+    activeMenu: '/example/list'  if set path, the sidebar will highlight the path you set
+  }
+ */
 
-//静态路由
+/**
+ * constantRoutes
+ * a base page that does not have permission requirements
+ * all roles can be accessed
+ */
 export const constantRoutes = [
   {
     path: '/login',
@@ -39,33 +61,33 @@ export const constantRoutes = [
       component: () => import('@/views/dashboard/index'),
       meta: { title: '首页', icon: 'dashboard' }
     }]
-  },
-  // 404 page must be placed at the end !!!
-]
+  }
 
-//动态路由
+  // 404 page must be placed at the end !!!
+
+]
+// 动态路由
 export const asyncRoutes = [
   departmentRouter,
-  approvalRouter,
-  attendanceRouter,
+  roleRouter,
   employeeRouter,
   permissionRouter,
-  roleRouter,
+  attendanceRouter,
+  approvalRouter,
   salaryRouter,
-  socialRouter,
-]
-
+  socialRouter]
 const createRouter = () => new Router({
   // mode: 'history', // require service support
   scrollBehavior: () => ({ y: 0 }),
-  routes: constantRoutes //默认引入静态路由
+  routes: constantRoutes // 默认引入静态路由
 })
 
 const router = createRouter()
 
+// Detail see: https://github.com/vuejs/vue-router/issues/1234#issuecomment-357941465
 export function resetRouter() {
   const newRouter = createRouter()
-  router.matcher = newRouter.matcher // 重置路由
+  router.matcher = newRouter.matcher // reset router
 }
 
 export default router
--- src/router/modules/employee.js
+++ src/router/modules/employee.js
@@ -1,8 +1,8 @@
 import layout from '@/layout'
 export default {
   path: '/employee',
-  name:'employee',
   component: layout,
+  name: 'employee',
   children: [{
     path: '',
     name: 'employee',
@@ -11,14 +11,14 @@ export default {
       title: '员工',
       icon: 'people'
     }
-  },{
-    path:'detail/:id?',
-    component:() => import('@/views/employee/detail.vue'),
-    hidden:true,
-    meta:{
-      title:'员工详情'
+  }, {
+    path: '/employee/detail/:id?', // 员工详情的地址
+    component: () => import('@/views/employee/detail.vue'),
+    hidden: true, // 表示隐藏在左侧菜单
+    meta: {
+      title: '员工详情' // 显示在导航的文本
     }
-  }
-
-]
+  }]
 }
+
+// 只想在左侧菜单显示一级菜单的话 让二级路由只有一个显示在左侧菜单
